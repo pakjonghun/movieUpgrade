238c4d0d1be625a407527b0c60e3c0ea1fc79b58
정훈박 <fireking5997@gmail.com>
2021-05-30 11:28:52 +0900

    modified

--- js/index.js
+++ js/index.js
@@ -3,37 +3,46 @@ const posting__button = document.querySelector("#posting__button");
 const show__postingBtn = document.querySelector("#show__posting");
 const main = document.querySelector("#main");
 
-$.ajax({
-  url: "http://spartacodingclub.shop/post",
-  type: "get",
-  dataType: "json",
-  data: "",
-  success: function (data) {
-    const { articles } = data;
+document.addEventListener("domcontentloaded",function(){
 
-    const tag = articles.map(
-      (item) =>
-        `
-        <article class="main__card">
-        <a class="link" href=${item["url"]}>
+})
 
-        <img
-          src=${item["image"]}
-          alt="card__img"
-          class="card__img"
-        />
-        <div class="card__info">
-          <p>${item["title"]}</p>
-          <p>${item["desc"]}</p>
-        </div>
-        </a>
-      </article>`
-    );
 
-    main.innerHTML = tag.join("");
-  },
-  error: function (request, status, error) {},
-});
+const getData=()=>{
+  $.ajax({
+    url: "http://spartacodingclub.shop/post",
+    type: "get",
+    dataType: "json",
+    data: "",
+    success: function (data) {
+      const { articles } = data;
+  
+      const tag = articles.map(
+        (item) =>
+          `
+          <article class="main__card">
+          <a class="link" href=${item["url"]}>
+  
+          <img
+            src=${item["image"]}
+            alt="card__img"
+            class="card__img"
+          />
+          <div class="card__info">
+            <p>${item["title"]}</p>
+            <p>${item["desc"]}</p>
+          </div>
+          </a>
+        </article>`
+      );
+  
+      main.innerHTML = tag.join("");
+    },
+    error: function (request, status, error) {},
+  });
+}
+
+
 
 const showPosting = () => {
   posting.classList.toggle("show");
